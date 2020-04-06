375f39588ab3554e4f0dfd7402bd3a02cbd00e3d
Jay Ferguson <fergujo@amazon.com>
2020-04-06 15:38:46 -0700

    Added create donations page and nav.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,7 +15,20 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/create_donation', methods=['GET', 'POST'])
+def create_donation():
+
+    if request.method == 'POST':
+        donor = Donor.get(Donor.name == request.form['name'].title())
+        donation = Donation(donor=donor, value=int(request.form['donation_value']))
+        donation.save()
+
+        return redirect(url_for('all'))
+    else:
+        return render_template('create_donation.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
